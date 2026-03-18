3791ab2c623bdfd94f70b706cbce531b5dfcf804
Chen Peng <chen56@msn.com>
2026-03-18 18:44:38 +0800

    feat: add all mono project test stub

---
+++ packages/dao-core/test/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+
+describe('dao-core', () => {
+  it('should pass', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ packages/dao-tui/test/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+
+describe('dao-tui', () => {
+  it('should pass', () => {
+    expect(true).toBe(true);
+  });
+});
